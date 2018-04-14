31b4ff32410e462676fb8a10342a6a4bb246a913
abha <abha.2903@gmail.com>
2018-04-14 19:00:17 +0530

    Create the PetDetails and PetTypes model

--- petshop/pets/models.py
+++ petshop/pets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class PetTypes(models.Model):
+    """
+    Model represent the type of the pet. Like: Dog, Cat, Rabbit..etc
+    """
+
+    pet_type = models.CharField(max_length=255, null=True, blank=True, verbose_name="Type of the pet")
+    is_active = models.BooleanField(default=True, verbose_name="Is pet type active")
+    created = models.DateTimeField(null=True, auto_now_add=True)
+    modified = models.DateTimeField(null=True, auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.pet_type)
+
+
+class PetDetails(models.Model):
+    """
+    Model Represent the details of the pet. Like: pet's name, dob etc..
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True, related_name="pet_owner")
+    name = models.CharField(max_length=255, null=True, blank=True, verbose_name="Name of the pet")
+    dob = models.DateField(null=True, blank=True, verbose_name="Date of birth of pet")
+    created = models.DateTimeField(null=True, auto_now_add=True)
+    modified = models.DateTimeField(null=True, auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.name if self.name else self.pk)
